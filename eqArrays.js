41b735045dcc9c16a45999d8c7c495ce2dba2a0b
Millington-Holi7 <holinas007@gmail.com>
2024-01-30 18:50:25 -0500

    Take in two arrays and returns true or false.

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+
+console.log(eqArrays(["1", "2", "4"], ["1", "2", "3"]));
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
