11e0cf98b2d1d798fb6ea53ee4b8adc9c60891d6
Millington-Holi7 <holinas007@gmail.com>
2024-01-31 18:13:08 -0500

    Comparing each letter, since asertEqual can't compare objects.

--- countLetters.js
+++ countLetters.js
@@ -1,4 +1,4 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -7,7 +7,7 @@ const assertEqual = function(actual, expected) {
 };
 //take in a sentence as a string
 // retrun a count of each letter as an object
-const countLetters = function(sentence) {
+const countLetters = function (sentence) {
   const count = {};
   for (const value of sentence) {
     if (count[value]) {
@@ -18,8 +18,15 @@ const countLetters = function(sentence) {
   }
   return count;
 };
+console.log(countLetters('hether'));
 
-console.log(countLetters('Heart'));
-assertEqual(countLetters('Heart'), { H: 1, e: 1, a: 1, r: 1, t: 1 }); // will not work as assertEqual can't compare objects. Only strings, numbers
+let result = countLetters('hether');
+assertEqual(result['h'], 2);
+assertEqual(result['e'], 2);
+assertEqual(result['r'], 1);
+assertEqual(result['t'], 1);
+
+
+//assertEqual(countLetters('Heart'), { H: 1, e: 1, a: 1, r: 1, t: 1 }); // will not work as assertEqual can't compare objects. Only strings, numbers
 //take in a sentence as a string
 // retrun a count of each letter as an object
