8fccbfd29b98de6e3cdc169f5b17e296ba8e9125
Millington-Holi7 <holinas007@gmail.com>
2024-01-31 17:44:32 -0500

    A function that takes a string and returns a count of each letter as an object.

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+//take in a sentence as a string
+// retrun a count of each letter as an object
+const countLetters = function(sentence) {
+  const count = {};
+  for (const value of sentence) {
+    if (count[value]) {
+      count[value] += 1;
+    } else {
+      count[value] = 1;
+    }
+  }
+  return count;
+};
+
+console.log(countLetters('Heart'));
+assertEqual(countLetters('Heart'), { H: 1, e: 1, a: 1, r: 1, t: 1 }); // will not work as assertEqual can't compare objects. Only strings, numbers
+//take in a sentence as a string
+// retrun a count of each letter as an object
