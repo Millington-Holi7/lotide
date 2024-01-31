22a38ac269b4cdb2f13c3dea34f183f1adfd628e
Millington-Holi7 <holinas007@gmail.com>
2024-01-30 23:08:56 -0500

    create a function that that puts nested arrays in one array.

---
+++ flatten.js
@@ -0,0 +1,35 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`💚💚💚 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const flatten = function(array) {
+  let mainArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (const item of array[i]) {
+        mainArray.push(item);
+      }
+    } else {
+      mainArray.push(array[i]);
+    }
+
+  }
+  return mainArray;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
