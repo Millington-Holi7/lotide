b3f7a5a0692b91219f76c60d6a532aa7d25eb600
Millington-Holi7 <holinas007@gmail.com>
2024-01-30 21:40:05 -0500

    fixed some vaiables that weren't Camelcase.

--- without.js
+++ without.js
@@ -1,4 +1,4 @@
-const eqArrays = function(arr1, arr2) {
+const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
@@ -8,7 +8,7 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-const assertArraysEqual = function(arr1, arr2) {
+const assertArraysEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(`💚💚💚 Assertion Passed: ${arr1} === ${arr2}`);
   } else {
@@ -17,14 +17,14 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 
-const without = function(array, exempt) {
-  const NewArray = [];
+const without = function (array, exempt) {
+  const newArray = [];
   for (const element of array) {
     if (!exempt.includes(element)) {
-      NewArray.push(element);
+      newArray.push(element);
     }
   }
-  return NewArray;
+  return newArray;
 };
 
 console.log(without([1, 2, 3], [1]));
