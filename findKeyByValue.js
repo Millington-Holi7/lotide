3202e08a0792b67c55a346271742e9c4bb947c32
Millington-Holi7 <holinas007@gmail.com>
2024-02-01 16:26:59 -0500

    fixed placement of comments.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,17 +1,17 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 
   }
-
-  // take in an object and the value that we need to find
-  //scan the object for the value that matches
-  // if no match found, return undefined.
-
 };
-const findKeyByValue = function(object, expected) {
+// take in an object and the value that we need to find
+//scan the object for the value that matches
+// if no match found, return undefined.
+
+
+const findKeyByValue = function (object, expected) {
   for (let key in object) {
     let value = object[key];
 
