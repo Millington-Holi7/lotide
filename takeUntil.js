be13455df07eeb0583c492a76c02f096bf5d129a
Millington-Holi7 <holinas007@gmail.com>
2024-02-05 16:26:40 -0500

    exclude the element that returns true from the callback.

--- takeUntil.js
+++ takeUntil.js
@@ -1,4 +1,4 @@
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
@@ -8,7 +8,7 @@ const eqArrays = function (arr1, arr2) {
   return true;
 };
 
-const assertArraysEqual = function (arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(`💚💚💚 Assertion Passed: ${arr1} === ${arr2}`);
   } else {
@@ -16,18 +16,21 @@ const assertArraysEqual = function (arr1, arr2) {
   }
 };
 
-const takeUntil = function (array, callback) {
+const takeUntil = function(array, callback) {
   const results = [];
+
 
   for (let i = 0; i < array.length; i++) {
     let data = array[i];
-    results.push(data);
+
     if (callback(data)) {
       break;
+    } else {
+      results.push(data);
     }
   }
   return results;
-}
+};
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
@@ -39,5 +42,5 @@ const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Re
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
-assertArraysEqual(results1, [1, 2, 5, 7, 2, -1])
-assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood'])
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
