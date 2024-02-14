8cbaf06e83dc91cde771a142634b166fa41cda50
Millington-Holi7 <holinas007@gmail.com>
2024-02-13 22:56:57 -0500

    Using modules to DRY up our code (sharing code between files)

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,10 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-};
+const eqArrays = require('../eqArrays.js');
 
-const assertArraysEqual = function(arr1, arr2) {
+const assertArraysEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(`💚💚💚 Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 4], [1, 2, 3]);
+module.exports = assertArraysEqual;
