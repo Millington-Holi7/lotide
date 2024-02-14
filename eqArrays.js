b92674ab94ae035b5685385ce482e3e96f66f81e
Millington-Holi7 <holinas007@gmail.com>
2024-02-13 21:50:35 -0500

    Using modules to DRY up our code (sharing code between files)

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   else {
@@ -20,8 +11,4 @@ const eqArrays = function(arr1, arr2) {
 };
 
 
-console.log(eqArrays(["1", "2", "4"], ["1", "2", "3"]));
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
