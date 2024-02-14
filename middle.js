14edb6deaa532c0ae95d58e109a0151237c5b810
Millington-Holi7 <holinas007@gmail.com>
2024-02-13 23:12:27 -0500

    Using modules to DRY up our code (sharing code between files)

--- middle.js
+++ middle.js
@@ -1,22 +1,3 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`💚💚💚 Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-
 const middle = function (array) {
 
   if (array.length <= 2) {
@@ -27,8 +8,6 @@ const middle = function (array) {
     return [array[(array.length / 2) - 1], array[array.length / 2]];
 
   }
-}
+};
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
+module.exports = middle;
